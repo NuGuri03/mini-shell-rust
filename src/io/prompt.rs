7721d66aa5ee3ba1546a519c41194a1680331384
yumin <sym2596@knu.ac.kr>
2025-06-26 17:22:37 +0900

    feat: add color in prompt

--- src/io/prompt.rs
+++ src/io/prompt.rs
@@ -1,8 +1,14 @@
 use std::io::{self, Write};
 
+use colored::Colorize;
+
 pub fn print_prompt() {
-    // TODO: add cuurent_path
-    // TODO: 
-    print!("mysh> ");
-    io::stdout().flush().expect("Failed to stdout");
+    let current_path = std::env::current_dir()
+        .expect("failed to get current dir")
+        .to_str()
+        .expect("failed to parse &str")
+        .to_string();
+
+    print!("{}:{}$ ", "host".red(), current_path.blue());
+    io::stdout().flush().expect("failed to stdout");
 }
