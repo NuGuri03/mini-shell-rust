1f8b5d0e84158f0536ced0b81394a20a24194894
yumin <sym2596@knu.ac.kr>
2025-06-26 17:35:31 +0900

    feat: add device name

--- src/io/prompt.rs
+++ src/io/prompt.rs
@@ -3,12 +3,19 @@ use std::io::{self, Write};
 use colored::Colorize;
 
 pub fn print_prompt() {
+    let username = whoami::username();
+    let host = whoami::devicename();
     let current_path = std::env::current_dir()
         .expect("failed to get current dir")
         .to_str()
         .expect("failed to parse &str")
         .to_string();
 
-    print!("{}:{}$ ", "host".red(), current_path.blue());
+    let user_host = format!("{}@{}", username, host).bold().red();
+    let path = current_path.blue();
+
+    let prompt = format!("{}:{}$ ", user_host, path);
+
+    print!("{}", prompt);
     io::stdout().flush().expect("failed to stdout");
 }
