0f51add81ed1ac0febeac6eb4e26b749862a9c25
Yumin <sym2596@knu.ac.kr>
2025-06-26 17:40:26 +0900

    feat/prompt (#4)
    * feat: add color in prompt
    
    * feat: add device name
    
    * refactor: replace .to_str() to .display()

--- src/io/prompt.rs
+++ src/io/prompt.rs
@@ -1,6 +1,20 @@
 use std::io::{self, Write};
 
+use colored::Colorize;
+
 pub fn print_prompt() {
-    print!("mysh> ");
-    io::stdout().flush().expect("Failed to stdout");
+    let username = whoami::username();
+    let host = whoami::devicename();
+    let current_path = std::env::current_dir()
+        .expect("failed to get current dir")
+        .display()
+        .to_string();
+
+    let user_host = format!("{}@{}", username, host).bold().red();
+    let path = current_path.blue();
+
+    let prompt = format!("{}:{}$ ", user_host, path);
+
+    print!("{}", prompt);
+    io::stdout().flush().expect("failed to stdout");
 }
